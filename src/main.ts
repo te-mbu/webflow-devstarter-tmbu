14408a0995ad22826eb06f6251e37fa39ed17724
Térence MBU <mbu.terence@gmail.com>
2025-04-04 13:06:05 +0200

    cleaning reveal component

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import type { Webflow as WebflowType } from '@finsweet/ts-utils';
 
-import { initFadeAnimations } from './components/scrollReveal';
-
 if (!window.Webflow) {
   const callbacks: Array<() => void> = [];
   const originalPush = callbacks.push.bind(callbacks);
@@ -19,11 +17,7 @@ export const Webflow = window.Webflow as WebflowType;
 document.addEventListener('DOMContentLoaded', () => {
   if (window.Webflow) {
     window.Webflow.push(() => {
-      initFadeAnimations('.fade-in', {
-        startY: 50,
-        duration: 1,
-        ease: 'power3.out',
-      });
+      console.log("Hello World!")
     });
   }
 });
